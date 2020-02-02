26d031e8ea9dda4c94001764707f2e19a06d3a18
annajankowska909 <60484932+annajankowska909@users.noreply.github.com>
2020-02-02 12:12:46 +0100

    Add new files

---
+++ openfile.py
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Anna Jankowska
+#
+# Created:     02.02.2020
+# Copyright:   (c) Anna Jankowska 2020
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+a = open("openfile.txt", "r")
+print(a.readline())
+a.close()
